2fa6b269d8167d00274fb966e9094be24b106e08
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-11-09 21:34:45 +0100

    switch from mongodb to mongoose - products

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -12,7 +12,6 @@ router.get('/departments', async (req, res) => {
 });
 
 router.get('/departments/random', async (req, res) => {
-
   try {
     const count = await Department.countDocuments();
     const rand = Math.floor(Math.random() * count);
@@ -23,11 +22,9 @@ router.get('/departments/random', async (req, res) => {
   catch(err) {
     res.status(500).json({ message: err });
   }
-
 });
 
 router.get('/departments/:id', async (req, res) => {
-
   try {
     const dep = await Department.findById(req.params.id);
     if(!dep) res.status(404).json({ message: 'Not found' });
@@ -36,27 +33,21 @@ router.get('/departments/:id', async (req, res) => {
   catch(err) {
     res.status(500).json({ message: err });
   }
-
 });
 
 router.post('/departments', async (req, res) => {
-
   try {
-
     const { name } = req.body;
     const newDepartment = new Department({ name: name });
     await newDepartment.save();
     res.json({ message: 'OK' });
-
   } catch(err) {
     res.status(500).json({ message: err });
   }
-
 });
 
 router.put('/departments/:id', async (req, res) => {
   const { name } = req.body;
-
   try {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
@@ -68,11 +59,9 @@ router.put('/departments/:id', async (req, res) => {
   catch(err) {
     res.status(500).json({ message: err });
   }
-
 });
 
 router.delete('/departments/:id', async (req, res) => {
-
   try {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
@@ -84,5 +73,4 @@ router.delete('/departments/:id', async (req, res) => {
   catch(err) {
     res.status(500).json({ message: err });
   }
-
 });

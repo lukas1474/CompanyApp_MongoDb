a7d390517d5c38a3b0f2457da0eb3344a74517f6
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-11-09 21:25:15 +0100

    switch from mongo to mongoose - employees

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    department: { type: String, required: true }
-  });
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  department: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Department', employeeSchema);

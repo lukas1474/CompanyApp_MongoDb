39c6b2e8e44344199c152acda5516d7d13f59035
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-11-09 22:09:25 +0100

    add show information about delete or change content from departments.routes.js

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -52,7 +52,7 @@ router.put('/departments/:id', async (req, res) => {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
       await Department.updateOne({ _id: req.params.id }, { $set: { name: name }});
-      res.json({ message: 'OK' });
+      res.json({ message: `Department: ${_id} has been changed` });
     }
     else res.status(404).json({ message: 'Not found...' });
   }
@@ -66,7 +66,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await(Department.findById(req.params.id));
     if(dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      res.json({ message: `Department: ${_id} has been removed` });
     }
     else res.status(404).json({ message: 'Not found...' });
   }
